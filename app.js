1c0e09b9a3259b1a5fda805747bcf63bb694b68c
davidabuu <65656238+davidabuu@users.noreply.github.com>
2020-12-31 12:16:25 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,104 +1,101 @@
-// Getting the Question elememts
-const question = document.querySelectorAll('#question1');
-//Increment the score
+//Get the UI Elemnts
+const questionForm = document.querySelector('form');
+const body = document.querySelector('.body');
+//Get the answers in an array = 
+const testAnswers = ['A', 'B', 'A', 'B', 'D', 'A', 'D', 'A', 'C', 'B'];
+const resultContainer = document.querySelector('.result-container');
+const correction = document.querySelector('.correction');
+const showScore = document.querySelector('.show-score');
+let timed = 0;
 let score = 0;
-// Getting the Submit Button
-const button = document.querySelector('.submit');
-
-// Adding an Event Listener
-button.addEventListener('click', (e) => {
-    e.preventDefault();
-    //Using a for Each loop
-    question.forEach((check) => {
-        // Check correct answer
-      if(check.value === 'Brendan Eich' &  check.checked === true){
-         score++;
-        document.querySelector('.passed').innerHTML = '&#9989';
-        document.querySelector('.failed').style.display = 'none';
-      }else{
-        document.querySelector('.failed').innerHTML = '&#10060';
-      }
-      if(check.value === 'function addNum()' &  check.checked === true){
-            score++;
-        document.querySelector('.passed2').innerHTML = '&#9989';
-        document.querySelector('.failed2').style.display = 'none';
-      }else{
-        document.querySelector('.failed2').innerHTML = '&#10060';
-      }
-      if(check.value === 'console.log()' &  check.checked === true){
-        score++;
-        document.querySelector('.passed3').innerHTML = '&#9989';
-        document.querySelector('.failed3').style.display = 'none';
-       }else{
-        document.querySelector('.failed3').innerHTML = '&#10060';
-      }
-       if(check.value === 'True' &  check.checked === true){
-        score++;
-        document.querySelector('.passed4').innerHTML = '&#9989';
-        document.querySelector('.failed4').style.display = 'none';
-       }else{
-        document.querySelector('.failed4').innerHTML = '&#10060';
-      }
-       if(check.value === 'Browser' &  check.checked === true){
-        score++;
-        document.querySelector('.passed5').innerHTML = '&#9989';
-        document.querySelector('.failed5').style.display = 'none';
-       }else{
-        document.querySelector('.failed5').innerHTML = '&#10060';
-      }
-       if(check.value === 'for(let i = 0; i > 0; i++);' &  check.checked === true){
-        score++;
-        document.querySelector('.passed6').innerHTML = '&#9989';
-        document.querySelector('.failed6').style.display = 'none';
-       }else{
-        document.querySelector('.failed6').innerHTML = '&#10060';
-      }
-       if(check.value === 'forEach()' &  check.checked === true){
-        score++;
-        document.querySelector('.passed7').innerHTML = '&#9989';
-        document.querySelector('.failed7').style.display = 'none';
-       }else{
-        document.querySelector('.failed7').innerHTML = '&#10060';
-      }
-       if(check.value === 'using typeOf' &  check.checked === true) {
-        score++;
-        document.querySelector('.passed8').innerHTML = '&#9989';
-        document.querySelector('.failed8').style.display = 'none';
-       }else{
-        document.querySelector('.failed8').innerHTML = '&#10060';
-      }
-       if(check.value === '6' &  check.checked === true){
-        score++;
-        document.querySelector('.passed9').innerHTML = '&#9989';
-        document.querySelector('.failed9').style.display = 'none';
-       }else{
-        document.querySelector('.failed9').innerHTML = '&#10060';
-      }
-       if(check.value === 'toUpperCase()' &  check.checked === true){
-        score++;
-        document.querySelector('.passed10').innerHTML = '&#9989';
-        document.querySelector('.failed10').style.display = 'none';
-       }else{
-        document.querySelector('.failed10').innerHTML = `&#10060`;
-        
-      }            
+//Show correct answer
+function showCorrectAnswer(){
+    const good = document.querySelectorAll('.fa-check')
+    good.forEach(mark => {
+        mark.classList.add('show');
     })
-    // Cacculate the Score
-    let total = score++
-    document.querySelector('.total').innerHTML = `Total Score: ${total} out of 10`
-    if(total === 10){
-      score = 10;
-      total = score;
+}
+// Show wrong answer
+function showWrongAnswer(){
+    const failed = document.querySelectorAll('.fa-times');
+    failed.forEach((fail, index)=> {
+        fail.classList.add('show')
+    })
+}
+
+//Add event listener on the form button
+questionForm.addEventListener('submit', e => {
+    e.preventDefault();
+    // Get the values in an array
+let answer = [questionForm.q1.value, questionForm.q2.value,questionForm.q3.value, questionForm.q4.value, questionForm.q5.value, questionForm.q6.value, questionForm.q7.value, questionForm.q8.value, questionForm.q9.value, questionForm.q10.value];
+    answer.forEach((ans, index) => {
+        if(ans === testAnswers[index]){
+            score++;
+            showScore.innerHTML = score;
+            //Show correct answer
+            const good = document.querySelectorAll('.fa-check');
+            good[index].classList.add('show');
+        }
+        else{
+            const failed = document.querySelectorAll('.fa-times');
+            failed[index].classList.add('show');
+        }
+    });
+    //Display score to DOM
+showScore.innerHTML = `Total Score:${score} out of 10`;
+// Show result
+resultContainer.classList.add('show');
+//Show correction
+document.querySelector('.body').style.display = 'none';
+correction.addEventListener('click', showCorrection)
+function showCorrection(){
+    resultContainer.classList.remove('show');
+    document.querySelector('.body').style.display = 'block';
+}
+});
+const startingTime =1;
+let time = startingTime * 60;
+//Timing function
+window.addEventListener('DOMContentLoaded', () => {
+const countDown = document.querySelector('.time');
+const timeInterVal = setInterval(timeCountDown, 1000)
+function timeCountDown(){
+    const minutes = Math.floor(time / 60);
+    let seconds = (time % 60);
+   seconds = seconds < 10 ? '0' + seconds : seconds 
+    countDown.innerHTML = `${minutes}: ${seconds}`
+    time--;
+    if(time < 0){
+        time = 0;
+        time = timed;
+        resultContainer.classList.add('show');
+        document.querySelector('.body').style.display = 'none';
+        showScore.innerHTML = `Your Time is Up`
+        clearInterval(timeInterVal)
+        
+        
+            
     }
-    score = 0
-    // Check Answers
-    let answer  = document.querySelector('.answer').innerHTML = 'Click Here To Check For Answers'
-})
-// Repeat the quiz
-const repeat = document.querySelector('.repeat');
-repeat.addEventListener('click', (e) => {
-  e.preventDefault()
-  window.location.reload()
-})
+} 
+});
+if(timed === 0){
+correction.addEventListener('click', () => {
+    resultContainer.classList.remove('show');
+    document.querySelector('.body').style.display = 'block';
+    let answer = [questionForm.q1.value, questionForm.q2.value,questionForm.q3.value, questionForm.q4.value, questionForm.q5.value, questionForm.q6.value, questionForm.q7.value, questionForm.q8.value, questionForm.q9.value, questionForm.q10.value];
+answer.forEach((ans, index) => {
+if(ans === testAnswers[index]){
+    score++;
+    //Show correct answer
+    const good = document.querySelectorAll('.fa-check');
+    good[index].classList.add('show');
+}
+else{
+    const failed = document.querySelectorAll('.fa-times');
+    failed[index].classList.add('show');
+}
+document.querySelector('.score').innerHTML = `Your Total Score is ${score} out of 10`
+});
 
-
+})
+}
